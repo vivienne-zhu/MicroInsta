91681c3348e7c9f9427450be388a28d4a75ee945
VvivienneZhu <vivienne.zhujy@gmail.com>
2020-06-10 00:07:28 -0700

    Init

---
+++ MicroInsta/MicroInsta/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MicroInsta
+//
+//  Created by Vivienne Zhu on 6/10/20.
+//  Copyright © 2020 Vivienne Zhu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
