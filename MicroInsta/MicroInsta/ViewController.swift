c518774bec4f5dbf916ca95e72dae36c02e2b8cd
VvivienneZhu <vivienne.zhujy@gmail.com>
2020-06-10 23:35:41 -0700

    Updates to SignIn and Register Function

--- MicroInsta/MicroInsta/ViewController.swift
+++ MicroInsta/MicroInsta/ViewController.swift
@@ -7,6 +7,9 @@
 //
 
 import UIKit
+import Firebase
+import FirebaseAuth
+
 
 class ViewController: UIViewController {
     
@@ -16,16 +19,36 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
+    
+    
+    // This method directs users to the feed page if signed in successfully.
+    // Else an error message is displayed.
     @IBAction func signInClicked(_ sender: UIButton) {
+        if emailText.text != "" && passwordText.text != ""{
+            Auth.auth().signIn(withEmail: emailText.text!, password: passwordText.text!) { (authdata, error) in
+                if error != nil {
+                ViewController().makeAlert(titleInput: "Error!", messageInput: error?.localizedDescription ?? "Error")
+                } else {
+                    self.performSegue(withIdentifier: "toFeedVC", sender: nil)
+                }
+            }
+        } else {
+            self.makeAlert(titleInput: "Error", messageInput: "Invalid Email/Password")
+        }
     }
     
     
     @IBAction func registerClicked(_ sender: UIButton) {
+        performSegue(withIdentifier: "toRegisterVC", sender: nil)
     }
     
+    func makeAlert(titleInput: String, messageInput: String){
+        let alert  = UIAlertController(title: titleInput, message: messageInput, preferredStyle: UIAlertController.Style.alert)
+        let okButton = UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil)
+        alert.addAction(okButton)
+        self.present(alert, animated: true, completion: nil)
+    }
     
     
 }
