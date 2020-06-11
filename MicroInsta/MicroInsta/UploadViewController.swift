f03e607202f87fb2fd6f1efa7dbf0c24e24d3d33
VvivienneZhu <vivienne.zhujy@gmail.com>
2020-06-10 23:35:51 -0700

    Create UploadViewController.swift

---
+++ MicroInsta/MicroInsta/UploadViewController.swift
@@ -0,0 +1,30 @@
+//
+//  UploadViewController.swift
+//  MicroInsta
+//
+//  Created by Vivienne Zhu on 6/10/20.
+//  Copyright © 2020 Vivienne Zhu. All rights reserved.
+//
+
+import UIKit
+
+class UploadViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
