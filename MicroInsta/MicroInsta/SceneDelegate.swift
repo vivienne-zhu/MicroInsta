4ec69a454dcef8916b0cc0876152e8b142a4303c
VvivienneZhu <vivienne.zhujy@gmail.com>
2020-06-10 23:36:12 -0700

    Handle Login users

--- MicroInsta/MicroInsta/SceneDelegate.swift
+++ MicroInsta/MicroInsta/SceneDelegate.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import FirebaseAuth
+import Firebase
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
@@ -17,6 +19,13 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
+        
+        let currentUser = Auth.auth().currentUser
+        if currentUser != nil{
+            let board = UIStoryboard(name: "Main", bundle: nil)
+            let tabBar = board.instantiateViewController(withIdentifier: "tabbar") as! UITabBarController
+            window?.rootViewController = tabBar
+        }
         guard let _ = (scene as? UIWindowScene) else { return }
     }
 
