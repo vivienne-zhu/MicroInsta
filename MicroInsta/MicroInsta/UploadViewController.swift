0827aff4bcb2fcefc89f736f1723b44d230b4c62
VvivienneZhu <vivienne.zhujy@gmail.com>
2020-06-13 20:43:16 -0700

    Setup uplaodVC

--- MicroInsta/MicroInsta/UploadViewController.swift
+++ MicroInsta/MicroInsta/UploadViewController.swift
@@ -9,22 +9,22 @@
 import UIKit
 
 class UploadViewController: UIViewController {
-
+    @IBOutlet weak var imageView: UIImageView!
+    
+    @IBOutlet weak var commentText: UITextField!
+    
+    @IBOutlet weak var postButton: UIButton!
+   
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        imageView.isUserInteractionEnabled = true;
     }
     
+    
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func postButtonClicked(_ sender: UIButton) {
+        
     }
-    */
-
+    
 }
