32f14eedce089cf072cbeba1094e5cccc97c123b
VvivienneZhu <vivienne.zhujy@gmail.com>
2020-06-14 00:33:17 -0700

    Add upload function

--- MicroInsta/MicroInsta/UploadViewController.swift
+++ MicroInsta/MicroInsta/UploadViewController.swift
@@ -7,20 +7,38 @@
 //
 
 import UIKit
+import Firebase
 
-class UploadViewController: UIViewController {
-    @IBOutlet weak var imageView: UIImageView!
+class UploadViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {    
+@IBOutlet weak var imageView: UIImageView!
+
+@IBOutlet weak var commentText: UITextField!
+
+@IBOutlet weak var uploadButton: UIButton!
+
+override func viewDidLoad() {
+    super.viewDidLoad()
+
+    imageView.isUserInteractionEnabled = true
+    let gestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(chooseImage))
+    imageView.addGestureRecognizer(gestureRecognizer)
     
-    @IBOutlet weak var commentText: UITextField!
+}
+
+@objc func chooseImage() {
     
-    @IBOutlet weak var postButton: UIButton!
-   
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        imageView.isUserInteractionEnabled = true;
-    }
+    let pickerController = UIImagePickerController()
+    pickerController.delegate = self
+    pickerController.sourceType = .photoLibrary
+    present(pickerController, animated: true, completion: nil)
     
+}
+
+
+func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+    imageView.image = info[.originalImage] as? UIImage
+    self.dismiss(animated: true, completion: nil)
+}
     
 
     @IBAction func postButtonClicked(_ sender: UIButton) {
